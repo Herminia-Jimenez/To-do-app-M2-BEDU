49bf6a01efd8bb48221330dba609c218fccd549e
Grufty295 <sebastian.audetat.f@gmail.com>
2021-02-07 01:34:31 -0700

    Task Add: Add new to do logic

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // Shorthand para el objeto document.
 const d = document;
 
+// Seleccion de contenedor padre de la aplicacion.
+const appContainer = d.getElementById("app");
+addClasses(appContainer, "container-lg", "mx-4", "py-4", "bg-primary");
+
 // Funcion que crea un elemento nodo a partir de su tipo.
 function createNode(typeNode = "div") {
   return d.createElement(typeNode);
@@ -24,30 +28,52 @@ function addClasses(node, ...styleClasses) {
   });
 }
 
-const appContainer = d.getElementById("app");
-addClasses(appContainer, "container-lg", "mx-4", "py-4", "bg-primary");
-
-// Bloque de codigo para integrar cabecera de la app en html
-const appHeader = createNode("header");
-addClasses(appHeader, "container-fluid", "mb-4");
+// Bloque para integrar cabecera de la app en html
+const appHeader = createNode("form");
 appHeader.id = "appHeader";
+addClasses(appHeader, "container-fluid", "mb-4");
 
-const appInputTask = createNode("input");
-addClasses(appInputTask, "form-control", "mr-4");
-appInputTask.type = "text";
-appInputTask.placeholder = "Add a new task...";
+const inputTask = createNode("input");
+addClasses(inputTask, "form-control", "mr-3");
+inputTask.type = "text";
+inputTask.name = "todo";
+inputTask.placeholder = "Add a new task...";
 
-const appSubmitTask = createNode("button");
-addClasses(appSubmitTask, "btn", "btn-info");
-appSubmitTask.type = "submit";
-appendChildNodes(appSubmitTask, "Add");
-
-// Bloque de codigo para integrar el cuerpo de la app en html
-const appBody = createNode("main");
-addClasses(appBody, "container-fluid");
+const submitTask = createNode("button");
+addClasses(submitTask, "btn", "btn-info", "px-5");
+submitTask.type = "submit";
+appendChildNodes(submitTask, "Add");
 
 // Adicion de nodos hijos al header
-appendChildNodes(appHeader, appInputTask, appSubmitTask);
+appendChildNodes(appHeader, inputTask, submitTask);
+
+// Bloque para integrar el cuerpo de la app en html
+const appBody = createNode("main");
+appBody.id = "todo-list";
+addClasses(appBody, "container-fluid", "bg-active", "py-2");
+
+// Logica de adicion de todos
+function generateToDoTemplate(toDoDescription) {
+  const toDoTemplate = `
+    <div class="todo-item row shadow w-100  bg-light ">
+        <input class="checkToDo col" type="checkbox">
+        <div class="descriptionWrapper col-10 border-secondary"> 
+            ${toDoDescription} 
+        </div>
+        <button class="btn deleteToDo col"><img src="./delete.svg"></button>
+    </div>
+    `;
+  appBody.innerHTML += toDoTemplate;
+}
+
+appHeader.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const newToDo = e.target.todo.value.trim();
+  if (newToDo.length) {
+    generateToDoTemplate(newToDo);
+    e.target.reset();
+  }
+});
 
 // Adicion de nodos hijos a la aplicacion
 appendChildNodes(appContainer, appHeader, appBody);
